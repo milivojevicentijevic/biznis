0141785354d531fc894401893ad8710aa172e7bf
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-11 21:57:48 +0100

    partials

---
+++ partials/footer.php
@@ -0,0 +1,24 @@
+<footer>
+        <div class="container">
+            <div class="col">
+                <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; Consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-envelope"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-phone"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
