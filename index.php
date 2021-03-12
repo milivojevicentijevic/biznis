0f605616f44f6e8a3a2116430ddacec204cc9f09
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-12 20:52:04 +0100

    headings html

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
         <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
         <button>About us</button>
     </header>
+    <!-- headings -->
+    <section class="headings container">
+        <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2 class="titles"><span>We can</span> help your business grow</h2>
+            <h4>Get your business in 4 easy steps</h4>
+        </article>
+    </section>
 
 <?php include "partials/footer.php"; ?>    
     
