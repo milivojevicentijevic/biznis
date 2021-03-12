e1e01ba1346daca8660cd19c3f1d1c4167068776
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-12 20:19:32 +0100

    header html

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php include "partials/header.php"; ?>
 
-    <h1>Ovde cemo sutra content</h1>
+    <!-- header -->
+    <header>
+        <h1>Trusted and <br> professional advisers</h1>
+        <h3>For your business</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+        <button>About us</button>
+    </header>
 
 <?php include "partials/footer.php"; ?>    
     
