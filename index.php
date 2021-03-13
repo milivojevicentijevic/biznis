691cfa4d8da23a3d8e9c8f17072ce15db6598276
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-13 15:15:51 +0100

    headings html

--- index.php
+++ index.php
@@ -7,5 +7,17 @@
         <button>About us</button>
     </header>
 
+    <!-- headings -->
+    <section class="headings container">
+        <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2><span>WE CAN</span> WE CAN HELP YOUR BUSINESS GROW</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
 <?php include "partials/footer.php"; ?>    
     
