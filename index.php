0471fcaf611cb0862b1aa387c62dfae87548b52e
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-13 07:18:30 +0100

    whole contnent again

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php include "partials/header.php"; ?>
 
-    <!-- header -->
-    <header>
-        <h1>Trusted and <br> professional advisers</h1>
-        <h3>For your business</h3>
-        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
-        <button>About us</button>
-    </header>
-    <!-- headings -->
-    <section class="headings container">
-        <article>
-            <h2>01</h2>
-        </article>
-        <article>
-            <h3>About us</h3>
-            <h2 class="titles"><span>We can</span> help your business grow</h2>
-            <h4>Get your business in 4 easy steps</h4>
-        </article>
-    </section>
+    <h1>Krecemo content ponovo</h1>
 
 <?php include "partials/footer.php"; ?>    
     
