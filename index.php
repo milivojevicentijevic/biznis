b00d3493d4440897d402fda2243c096dca52df12
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-13 16:06:03 +0100

    services html

--- index.php
+++ index.php
@@ -42,5 +42,44 @@
         </article>
     </section>
 
+    <!-- services -->
+    <section class="services container">
+        <article>
+            <i class="fas fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-filter"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullhorn"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+    </section>
+
 <?php include "partials/footer.php"; ?>    
-    
